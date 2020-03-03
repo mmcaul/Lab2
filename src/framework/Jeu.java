de8893f814387dbf63a6285262008c1c1ec21efb
mmcaul <melissa.mcauley27@gmail.com>
2020-03-03 14:47:11 -0500

    test

--- src/framework/Jeu.java
+++ src/framework/Jeu.java
@@ -1,18 +1,79 @@
 package framework;
 
-public class Jeu {
+import java.util.Scanner;
 
-    private DeIterator deIterator;
-    private JoueurIterator joueurIterator;
-    private GameStrategy strategieJeu;
-    private int numTours;
+public abstract class Jeu implements GameStrategy{
 
-    public Jeu(CollectionDes collectionDes, CollectionJoueur collectionJoueur,
-               int numTours, GameStrategy strategieJeu){
-        deIterator = collectionDes.createIterator();
-        joueurIterator = collectionJoueur.createIterator();
-        this.numTours = numTours;
-        this.strategieJeu = strategieJeu;
+    Scanner sc = new Scanner(System.in);
+    int nbToursFait = 0;
+    int nbJoueurs, nbDes, nbFaceDe, nbTours;
+    CollectionJoueur tabNbJoueurs;
+    JoueurIterator joueurIterator;
+    CollectionDes tabNbDes;
+    DeIterator desIterator;
+
+    public abstract void calculerScoreTour();
+    public abstract Joueur calculerLeVaiqueur();
+
+
+    public final void commencerJeu(){
+
+        setNbJoueurs();
+        setNbFaceDe();
+        setNbTours();
+
+        while(nbToursFait != nbTours){
+
+            for(int i=0;i<nbJoueurs; i++){
+                calculerScoreTour();
+            }
+            calculerLeVaiqueur();
+            nbToursFait++;
+        }
     }
 
+    public void setNbJoueurs(){
+        System.out.println("Combien de joueurs veulent jouer?");
+        System.out.print(">>");
+
+        if(sc.hasNextInt()){
+            nbJoueurs = sc.nextInt();
+            tabNbJoueurs = new CollectionJoueur(nbJoueurs);
+
+            for(int i=0; i<nbJoueurs; i++){
+                tabNbJoueurs.addJoueur(new Joueur(i));
+            }
+            joueurIterator = tabNbJoueurs.createIterator();
+        }
+    }
+
+    public void setNbFaceDe(){
+        System.out.println("Combien de dés allez-vous utiliser?");
+        System.out.print(">>");
+
+        if(sc.hasNextInt()){
+            nbDes = sc.nextInt();
+            tabNbDes = new CollectionDes(nbDes);
+
+            System.out.println("Combien de faces ont vos dés?");
+            System.out.print(">>");
+
+            if(sc.hasNextInt()){
+                nbFaceDe = sc.nextInt();
+            }
+
+            for(int i=0; i<nbDes; i++){
+                tabNbDes.addDe(new De(nbFaceDe));
+            }
+            desIterator = tabNbDes.createIterator();
+        }
+    }
+
+    public void setNbTours(){
+        System.out.println("Combien de tours voulez-vous jouer?");
+        System.out.print(">>");
+        if(sc.hasNextInt()){
+            nbTours = sc.nextInt();
+        }
+    }
 }
